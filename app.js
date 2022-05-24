801046de350339092829e0ada5007e3b7a160eab
Adarsh Modi <adasanvin12345@gmail.com>
2022-05-24 19:07:47 +0530

    constants updated

--- app.js
+++ app.js
@@ -12,41 +12,26 @@ var porterStemmer = require('@stdlib/nlp-porter-stemmer')
 // const SpellCheck = require('@caijs/spellcheck')
 var spelling = require('spelling'),
   dictionary = require('spelling/dictionaries/en_US.js')
-
 var dict = new spelling(dictionary)
-// const { result } = require('lodash')
 
-// for (let i = 1; i <= 450; i++) {
+//reading the contents from the constants folder
+
 let final_kw = fs.readFileSync(__dirname + '/constants/keywords.txt', {
   encoding: 'utf-8',
 })
-// ps = ps.toString()
-// }
-
 final_kw = final_kw.split('\n')
-// console.log(final_kw.length)
 let idf = fs.readFileSync(__dirname + '/constants/idf.txt', {
   encoding: 'utf-8',
 })
-// ps = ps.toString()
-// }
-
 idf = idf.split('\n')
-// console.log(idf.length)
-
 let tfidf = fs.readFileSync(__dirname + '/constants/tfidf.txt', {
   encoding: 'utf-8',
 })
-// ps = ps.toString()
-// }
-
 tfidf = tfidf.split('\n')
-// console.log(tfidf.length)
 let ps_titles = fs.readFileSync(__dirname + '/dataset/problem_titles.txt', {
   encoding: 'utf-8',
 })
 ps_titles = ps_titles.split('\n')
-
 let ps_difficulties = fs.readFileSync(
   __dirname + '/dataset/problem_difficulties.txt',
   {
@@ -54,7 +39,6 @@ let ps_difficulties = fs.readFileSync(
   }
 )
 ps_difficulties = ps_difficulties.split('\n')
-
 let ps_submissions = fs.readFileSync(
   __dirname + '/dataset/problem_submissions.txt',
   {
@@ -62,22 +46,17 @@ let ps_submissions = fs.readFileSync(
   }
 )
 ps_submissions = ps_submissions.split('\n')
-
 let ps_urls = fs.readFileSync(__dirname + '/dataset/problem_urls.txt', {
   encoding: 'utf-8',
 })
 ps_urls = ps_urls.split('\n')
-
 let ps_codes = fs.readFileSync(__dirname + '/dataset/problem_codes.txt', {
   encoding: 'utf-8',
 })
 ps_codes = ps_codes.split('\n')
 
-// console.log(ps_codes.length)
-// console.log(tfidf)
 // express app
 const app = express()
-// app.listen(3000)
 // connect to mongodb & listen for requests
 const dbURI =
   'mongodb+srv://adarsh_modi_01:adarsh_modi_01@nodetuts.ei3k1.mongodb.net/node-tuts2?retryWrites=true&w=majority'
@@ -152,11 +131,11 @@ top10_blogs_copy = []
 // routes
 
 app.get('/', (req, res) => {
-  // res.redirect('/blogs')
-  console.log('index reached')
-  res.redirect('/blogs')
+  console.log('search reached')
+  res.redirect('/create')
 })
 
+// feebacks page
 app.get('/feedbacks', (req, res) => {
   Fb.find({})
     .sort({ createdAt: -1 })
@@ -166,7 +145,6 @@ app.get('/feedbacks', (req, res) => {
     .catch((err) => {
       console.log(err)
     })
-  // res.render('feedback', { title: 'All Feedbacks' })
   console.log('/feedbacks reached')
 })
 
@@ -183,7 +161,7 @@ app.get('/blogs', (req, res) => {
   Blog.find({
     index_of_ps: { $in: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] },
   })
-    // .sort({ createdAt: -1 })
+    .sort({ createdAt: -1 })
     .then((result) => {
       res.render('index', { blogs: result, title: 'All Questions', query: '' })
     })
@@ -194,15 +172,12 @@ app.get('/blogs', (req, res) => {
 })
 
 function cleaner(pss) {
-  // pss = pss.split('Input')
-  // pss = pss[0]
   let punctuationless = pss.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '')
   pss = punctuationless.replace(/\s{2,}/g, ' ')
   pss = pss.replace(/\d+/g, '')
   pss = pss.replace(/\s+/g, ' ').trim()
   pss = pss.replace(',', ' ')
   pss = pss.replace(/[^\w\s]/g, '').toLowerCase()
-  // console.log(pss)
   return pss
 }
 
@@ -217,10 +192,10 @@ function return_query_tfidf(q) {
   })
   for (let j = 0; j < query_keywords.length; j++) {
     query_keywords[j] = porterStemmer(query_keywords[j])
-    // cur_keywords[i] = spellCheck.checkToken(cur_keywords[i], 1)
     if (dict.search(query_keywords[j])[0] != undefined)
       query_keywords[j] = dict.search(query_keywords[j])[0].word
   }
+  console.log(query_keywords)
   query_tf = []
   for (let j = 0; j < final_kw.length; j++) {
     query_tf.push(0)
@@ -232,63 +207,30 @@ function return_query_tfidf(q) {
     if (final_kw.indexOf(query_keywords[j]) != -1)
       query_tf[final_kw.indexOf(query_keywords[j])] += 1
   }
-  // console.log(query_tf)
-  for (let j = 0; j < final_kw.length; j++) {
-    query_tf[j] = query_tf[j] / query_keywords.length
-    if (query_tf[j] < 0) console.log('erjed')
-  }
-  // console.log(query_tf)
   query_tfidf = []
-  // query_tfidf.push(query_tf)
   for (let j = 0; j < final_kw.length; j++) {
     query_tfidf.push(query_tf[j])
-    query_tfidf[j] = query_tfidf[j] * idf[j]
+    query_tfidf[j] = (query_tfidf[j] * idf[j]) / query_keywords.length
   }
-  // console.log(query_tfidf)
   return query_tfidf
 }
 function calc_similarity(a, b) {
-  // console.log(a)
-  // console.log(b)
   sum = 0
   sum1 = 0
   sum2 = 0
   for (let i = 0; i < final_kw.length; i++) {
-    sum += a[i] * b[i]
-    sum1 += a[i] * a[i]
-    sum2 += b[i] * b[i]
+    sum += parseFloat(a[i] * b[i])
+    sum1 += parseFloat(a[i] * a[i])
+    sum2 += parseFloat(b[i] * b[i])
   }
   sum3 = Math.sqrt(sum1 * sum2)
-  // console.log(sum, sum1, sum2)
   if (sum3 == 0) return 0
-  return sum / sum3
+  return parseFloat(sum / sum3)
 }
 function top10(q) {
   query_tfidf = return_query_tfidf(q)
 
   all_blogs = []
-  // const curBlog = Blog.find({}).exec()
-  // console.log(curBlog)
-  // Blog.find({}, (err, blog) => {
-  //   if (err) console.log(err)
-  //   // console.log(blog)
-  //   // return []
-  //   // console.log(blog.length)
-  //   for (let j = 0; j < blog.length; j++) {
-  //     similarity_value = calc_similarity(query_tfidf, blog[j].tfidf)
-  //     if (similarity_value != 'Nan')
-  //       all_blogs.push([similarity_value, blog[j]._id])
-  //     // console.log(similarity_value, blog[j]._id)
-  //   }
-  //   // console.log(all_blogs)
-  //   all_blogs.sort((a, b) => (a[0] > b[0] ? -1 : 1))
-  //   // console.log(all_blogs)
-  //   top10_blogs = []
-  //   for (let i = 0; i < 10; i++) {
-  //     top10_blogs.push(all_blogs[i][1])
-  //   }
-  // })
-  // top10_blogs = []
   for (let i = 0; i < 1450; i++) {
     cur_tfidf = []
     let sum = 0
@@ -298,12 +240,9 @@ function top10(q) {
     }
     // console.log(sum)
     similarity_value = calc_similarity(query_tfidf, cur_tfidf)
-    // all_blogs.push([similarity_value, blog.params.id])
     all_blogs.push([similarity_value, i])
   }
-  // // console.log(all_blogs)
   all_blogs.sort((a, b) => (a[0] > b[0] ? -1 : 1))
-  // // // console.log(all_blogs)
   top10_blogs = []
   for (let i = 0; i < 15; i++) {
     top10_blogs.push(all_blogs[i][1] + 1)
@@ -312,11 +251,6 @@ function top10(q) {
 }
 
 app.post('/blogs', (req, res) => {
-  // console.log(req.body);
-  // const blog = new Blog(req.body)
-  // console.log(req.body)
-  // console.log(req)
-
   if (req.body.category != undefined) {
     sort_by = req.body.category
     all_blogs = []
@@ -366,15 +300,12 @@ app.post('/blogs', (req, res) => {
   } else {
     let query_cleaned = req.body.query
     query_cleaned = cleaner(query_cleaned)
-
-    // console.log(query_cleaned)
     top10_blogs = top10(query_cleaned)
-    // console.log(top10_blogs)
     top10_blogs_copy = top10_blogs
 
     all_blogs = []
     Blog.find({ index_of_ps: { $in: top10_blogs } })
-      // .sort({ sort_by: -1 })
+      .sort({ sort_by: -1 })
       .then((blog) => {
         res.render('index', {
           blogs: blog,
@@ -386,13 +317,10 @@ app.post('/blogs', (req, res) => {
         console.log(err)
       })
   }
-
-  // console.log('copy', top10_blogs_copy)
 })
 
 function format_contraints(constraints) {
   constraints = constraints.split(' ')
-  // console.log(constraints)
   for (let i = 0; i < constraints.length; i++) {
     if (constraints[i] === '\\leq') constraints[i] = '<='
     if (constraints[i] === '\\geq') constraints[i] = '>='
@@ -406,8 +334,6 @@ function format_contraints2(constraints) {
     if (constraints[i][0] === ',') constraints[i] = constraints[i].substr(1)
     if (constraints[i] != '\r' && constraints[i].length > 2) {
       let curLen = constraints[i].length
-      // console.log(curLen)
-      // console.log(typeof constraints[i])
       let left = constraints[i].substr(0, (curLen - 1) / 2)
       let right = constraints[i].substr((curLen - 1) / 2, (curLen - 1) / 2)
       if (left === right) {
@@ -415,11 +341,9 @@ function format_contraints2(constraints) {
       }
     }
   }
-  // constraints = constraints.join(' ')
   return constraints
 }
 app.post('/feedbacks', (req, res) => {
-  // console.log(req.body);
   const fb = new Fb(req.body)
   setTimeout(() => {
     fb.save()
@@ -454,8 +378,6 @@ app.get('/blogs/:id', (req, res) => {
       }
       statement = statement2
       let ps = ''
-      // console.log(statement)
-      // statement = statement.split('Input Format')
       statement = statement.split('Constraints')
       ps = statement[0]
 
@@ -465,7 +387,6 @@ app.get('/blogs/:id', (req, res) => {
       if (statement === undefined) {
         res.render('error', { title: 'PAGE NOT FOUND' })
       }
-      // console.log(statement)
       if ((statement.match('Subtasks') || []).length != 0) {
         statement = statement.split('Subtasks')
         constraints = statement[0]
@@ -528,9 +449,6 @@ app.get('/blogs/:id', (req, res) => {
       if (statement === undefined) {
         res.render('error', { title: 'PAGE NOT FOUND' })
       }
-      // console.log(statement)
-
-      // console.log(ps)
 
       if ((statement.match('Output Format') || []).length != 0) {
         statement = statement.split('Output Format')
@@ -573,18 +491,6 @@ app.get('/blogs/:id', (req, res) => {
       console.log(err)
     })
 })
-
-// app.delete('/blogs/:id', (req, res) => {
-//   const id = req.params.id
-
-//   Blog.findByIdAndDelete(id)
-//     .then((result) => {
-//       res.json({ redirect: '/blogs' })
-//     })
-//     .catch((err) => {
-//       console.log(err)
-//     })
-// })
 
 // 404 page
 app.use((req, res) => {
